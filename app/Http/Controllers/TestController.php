5de3c7102f29420de0875ce66227ba916b79d723
cometure <yuya.m.89.0898@gmail.com>
2022-03-18 01:27:15 +0900

    テスト

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,17 +6,12 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    public function index(Request $request)
+  public function index()
     {
-        return view('index', ['txt' => '']);
+        return view('index');
     }
-    public function post(Request $request)
+  public function post(Request $request)
     {
-        $validate_rule = [
-            'name' => 'required',
-            'email' => 'required|email',
-        ];
-        $this->validate($request, $validate_rule);
-        return view('index', ['txt' => '正しい入力です']);
+        return view('thanks');
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,27 +5,18 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>COACHTECH</title>
+  <title>問い合わせ</title>
 </head>
 
 <body>
-  <p>{{$txt}}</p>
-  <form action="/" method="POST">
-    <table>
-      @csrf
-      <tr>
-        <th>氏名</th>
-        <td>
-          <input type="text" name="name">
-        </td>
-      </tr>
-      <tr>
-        <th>メールアドレス</th>
-        <td>
-          <input type="email" name="email">
-        </td>
-      </tr>
-    </table>
+  <form action="thanks" method="POST">
+    @csrf
+    <dl>
+      <dt>氏名</dt>
+      <input type="text" name="name">
+      <dt>メールアドレス</dt>
+      <input type="email" name="email">
+    </dl>
     <input type="submit" value="送信する">
   </form>
 </body>
